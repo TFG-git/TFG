9f4106e34ddc2785d2f4e824c30a18728e704573
TFG <correotfg2425@gmail.com>
2025-03-07 13:02:06 +0100

    prueba Leandro

--- app/src/main/java/com/example/tfg_inicial/MainActivity.java
+++ app/src/main/java/com/example/tfg_inicial/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fragmentTransaction.add(R.id.llContenedorFragments, fragmentInicial);
         fragmentTransaction.commit();
 
-        Toast.makeText(this, "Prueba de José", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Prueba de Leandro", Toast.LENGTH_LONG).show();
     }
 
     @Override
